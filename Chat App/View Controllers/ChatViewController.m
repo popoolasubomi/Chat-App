9dab7498812647231d4f45323d351787ca0dba7a
popoolasubomi <popoolaogooluwasubomi@gmail.com>
2020-07-06 15:53:25 -0400

    Implemented Chat functionalities

--- Chat App/View Controllers/ChatViewController.m
+++ Chat App/View Controllers/ChatViewController.m
@@ -7,8 +7,15 @@
 //
 
 #import "ChatViewController.h"
+#import "ChatCell.h"
+#import <Parse/Parse.h>
 
-@interface ChatViewController ()
+@interface ChatViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@property (weak, nonatomic) IBOutlet UITextField *chatMessageField;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@property (nonatomic, strong) NSArray *posts;
 
 @end
 
@@ -16,17 +23,60 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    
+    self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    
+    [NSTimer scheduledTimerWithTimeInterval:2 target:self selector:@selector(onTimer) userInfo:nil repeats:true];
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)sendButton:(id)sender {
+    PFObject *chatMessage = [PFObject objectWithClassName:@"Message_fbu2020"];
+    chatMessage[@"user"] = PFUser.currentUser;
+    chatMessage[@"text"] = self.chatMessageField.text;
+    [chatMessage saveInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+        if (succeeded) {
+            NSLog(@"The message was saved!");
+        } else {
+            NSLog(@"Problem saving message: %@", error.localizedDescription);
+        }
+    }];
 }
-*/
+
+- (void)onTimer {
+    PFQuery *query = [PFQuery queryWithClassName:@"Message_fbu2019"];
+    query.limit = 20;
+    [query includeKey:@"user"];
+    [query orderByDescending:@"createdAt"];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *posts, NSError *error) {
+        if (posts != nil) {
+            self.posts = posts;
+            [self.tableView reloadData];
+        } else {
+            NSLog(@"%@", error.localizedDescription);
+        }
+    }];
+    
+}
+
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    ChatCell *cell = [tableView dequeueReusableCellWithIdentifier: @"ChatCell"];
+    
+    NSDictionary *chatMessage = self.posts[indexPath.row];
+    PFUser *user = chatMessage[@"user"];
+    if (user != nil) {
+        cell.usernameLabel.text = user.username;
+    } else {
+        cell.usernameLabel.text = @"🤖";
+    }
+    cell.messageLabel.text = chatMessage[@"text"];
+    return cell;
+}
+
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+   return self.posts.count;
+}
 
 @end
---
+++ Chat App/Views/ChatCell.m
@@ -0,0 +1,24 @@
+//
+//  ChatCell.m
+//  Chat App
+//
+//  Created by Ogo-Oluwasobomi Popoola on 7/6/20.
+//  Copyright © 2020 Ogo-Oluwasobomi Popoola. All rights reserved.
+//
+
+#import "ChatCell.h"
+
+@implementation ChatCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
